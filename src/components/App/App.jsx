335a6a202b61601deaf91c277d022ca752a4a1e2
Michael <mitru0217@gmail.com>
2022-11-27 23:38:21 +0200

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-lone-blocks */
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import './App.css';
@@ -8,23 +9,31 @@ import Filter from '../Filter';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: nanoid(10), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(10), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(10), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(10), name: 'Annie Copeland', number: '227-91-26' },
     ],
 
     filter: '',
   };
+
   addContacts = data => {
-    const contact = {
+    const contactNew = {
       id: nanoid(10),
       name: data.name,
       number: data.number,
     };
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
+    const findNameIndex = this.state.contacts.findIndex(
+      contact => contact.name === contactNew.name
+    );
+
+    if (findNameIndex < 0) {
+      return this.setState(({ contacts }) => ({
+        contacts: [contactNew, ...contacts],
+      }));
+    }
+    return alert(`${contactNew.name} is already in contacts`);
   };
 
   changeFilter = e => {
@@ -38,7 +47,9 @@ class App extends Component {
   getSavedNames = () => {
     const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.includes(normalizedFilter));
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
 
   deleteContact = contactId => {
@@ -46,17 +57,18 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
   render() {
     const { filter } = this.state;
 
     const filteredName = this.getSavedNames();
-
     return (
       <div className="Container">
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.addContacts} />
         <h2>Contacts</h2>
         <Filter value={filter} onChange={this.changeFilter} />
+
         <ContactsList
           contacts={filteredName}
           onDeleteContact={this.deleteContact}
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -11,6 +11,7 @@ class Form extends Component {
 
   handleChange = e => {
     const { name, value } = e.target;
+
     this.setState({ [name]: value });
   };
 
