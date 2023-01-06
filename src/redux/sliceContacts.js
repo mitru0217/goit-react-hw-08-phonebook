b41a451f1eff626153b504f8b32e2d3435bbc431
Michael <mitru0217@gmail.com>
2023-01-06 13:05:29 +0200

    Update sliceContacts.js

--- src/redux/sliceContacts.js
+++ src/redux/sliceContacts.js
@@ -6,6 +6,7 @@ const initialContacts = [
   { id: nanoid(10), name: 'Hermione Kline', number: '443-89-12' },
   { id: nanoid(10), name: 'Eden Clements', number: '645-17-79' },
   { id: nanoid(10), name: 'Annie Copeland', number: '227-91-26' },
+  { id: nanoid(10), name: 'Gretta Turnberg', number: '3377-98-27' },
 ];
 
 export const contactsSlice = createSlice({
